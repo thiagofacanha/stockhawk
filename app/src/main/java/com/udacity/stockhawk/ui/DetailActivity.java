984be627c35e31d4b878d3d20d55ebe7b4ecf39c
Thiago Façanha <thiago.pereira@eldorado.org.br>
2017-04-27 18:25:42 -0300

    Added values to graph. YahooFinances wasnt returning history. Updated its libs and is working

--- app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -1,16 +1,20 @@
 package com.udacity.stockhawk.ui;
 
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import com.udacity.stockhawk.R;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Comparator;
 import java.util.List;
 
 import yahoofinance.Stock;
@@ -51,23 +55,47 @@ public class DetailActivity extends AppCompatActivity {
                 fromCalendar.add(Calendar.DAY_OF_MONTH, -7);
                 Stock stock = YahooFinance.get(symbol, fromCalendar, toCalendar, Interval.DAILY);
                 histQuotes = stock.getHistory();
+                histQuotes.sort(new Comparator<HistoricalQuote>() {
+                    @Override
+                    public int compare(HistoricalQuote historicalQuote, HistoricalQuote t1) {
+                        if (historicalQuote.getDate() == null || t1.getDate() == null) {
+                            return 0;
+                        }
+                        return historicalQuote.getDate().compareTo(t1.getDate());
+                    }
+                });
             } catch (Exception e) {
             }
             return histQuotes;
         }
 
         protected void onPostExecute(final List<HistoricalQuote> result) {
-            if (result.size() == 7) {
-                GraphView graph = (GraphView) findViewById(R.id.graph);
-                LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
-                        new DataPoint(0, 1),
-                        new DataPoint(1, 0),
-                        new DataPoint(2, 2),
-                        new DataPoint(3, 0),
-                        new DataPoint(4, 1)
-                });
-                graph.addSeries(series);
+
+            GraphView graph = (GraphView) findViewById(R.id.graph);
+
+            LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
+            for (HistoricalQuote item : result) {
+                series.appendData(new DataPoint(item.getDate().getTimeInMillis(), item.getClose().doubleValue()), true, 10);
             }
+            series.setColor(Color.GREEN);
+            series.setThickness(8);
+            final SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd");
+            graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+                @Override
+                public String formatLabel(double value, boolean isValueX) {
+                    if (isValueX) {
+                        Calendar cal = Calendar.getInstance();
+                        cal.setTimeInMillis((long) value);
+                        return dateFormat.format(cal.getTime());
+                    } else {
+                        return super.formatLabel(value, isValueX);
+                    }
+                }
+            });
+
+            graph.addSeries(series);
+
         }
+
     }
 }
